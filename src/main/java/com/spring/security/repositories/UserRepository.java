2b5c75202c5e30a46dedb6a0e1b6111c7d707101
FiataliS <FiataliS@gmail.com>
2022-05-30 18:59:25 +0500

    Initial commit

---
+++ src/main/java/com/spring/security/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.spring.security.repositories;
+
+
+import com.spring.security.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
